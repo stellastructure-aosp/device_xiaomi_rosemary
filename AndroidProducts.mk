2f0d9141eb5a161b43ec8499439c1d03030693c0
Stella Bloom <windowz414@gnuweeb.org>
2024-03-21 06:46:20 +0100

    rosemary: Add TARGET_RELEASE to lunch combo
    * A14QPR2 change. :shrug:
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rosemary-user \
-    aosp_rosemary-userdebug \
-    aosp_rosemary-eng
+    aosp_rosemary-ap1a-user \
+    aosp_rosemary-ap1a-userdebug \
+    aosp_rosemary-ap1a-eng
